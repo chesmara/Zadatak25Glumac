f92c3bdce3286cdb0e312d5811e571e45ed6b3fd
chesmara <srdjan@ekoneimar.com>
2017-03-08 15:34:31 +0100

    sa posla

--- app/src/main/java/com/example/androiddevelopment/zadatak20glumac/db/DatabaseHelper.java
+++ app/src/main/java/com/example/androiddevelopment/zadatak20glumac/db/DatabaseHelper.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
-    private static final String DATABASE_NAME  = "orimlite.db";
+    private static final String DATABASE_NAME  = "ormlite.db";
     private static final int DATABASE_VERSION =1;
 
     private Dao<Glumac, Integer> mGlumacDao= null;
